9ba56a831d420c0bfe1cd4e377682abf348063d6
Yetterko <161580374+Yetterko@users.noreply.github.com>
2025-04-12 06:40:42 +1000

    refactor: move hardcoded values to constants in Main.java

--- src/main/java/com/qut/cab302_project_pomodora/Main.java
+++ src/main/java/com/qut/cab302_project_pomodora/Main.java
@@ -10,11 +10,23 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        int START_WIDTH = 1920;
+        int START_HEIGHT = 1080;
+        int MIN_WIDTH = 1067;
+        int MIN_HEIGHT = 600;
+        String TITLE = "CAB302: Project Pomodora";
+        String LAUNCH_PAGE_ADDRESS = "fxml/home.fxml";
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(LAUNCH_PAGE_ADDRESS));
+        Scene scene = new Scene(fxmlLoader.load(), START_WIDTH, START_HEIGHT);
+        stage.setResizable(true);
+        stage.setMinHeight(MIN_HEIGHT);
+        stage.setMinWidth(MIN_WIDTH);
+        stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
