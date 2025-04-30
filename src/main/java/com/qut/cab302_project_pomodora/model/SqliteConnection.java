f2f7ca98fcd74f8ed9aa928699a8cb312095dd87
m1chaelrQUT <n11278633@qut.edu.au>
2025-04-30 23:40:51 +1000

    feat: Implement Session Management unit tests - Added unit tests for the SessionManager class - Added methods to SqliteConnection to set up a test connection and reset the connection and class after testing is complete.

--- src/main/java/com/qut/cab302_project_pomodora/model/SqliteConnection.java
+++ src/main/java/com/qut/cab302_project_pomodora/model/SqliteConnection.java
@@ -6,13 +6,36 @@ import java.sql.SQLException;
 
 public class SqliteConnection {
     private static Connection instance = null;
+    private static boolean isTest = false;
 
+    /**
+     * Private constructor to prevent instantiation.
+     */
     private SqliteConnection() {
         throw new UnsupportedOperationException("Utility class");
     }
 
+    /**
+     * Sets the instance to a test instance.
+     */
+    public static void setTestInstance(Connection testConnection) {
+        instance = testConnection;
+        isTest = true;
+    }
+
+    /**
+     * Resets the instance to null and sets isTest to false.
+     */
+    public static void reset() {
+        instance = null;
+        isTest = false;
+    }
+
+    /**
+     * Sets the instance to a test instance.
+     */
     public static Connection getInstance() {
-        if (instance == null) {
+        if (instance == null && !isTest) {
             String url = "jdbc:sqlite:pomodora.db";
             try {
                 instance = DriverManager.getConnection(url);
