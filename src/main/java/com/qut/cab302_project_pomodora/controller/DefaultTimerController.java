91571785114775785e3438c77f4219633828d5ee
Yetterko <161580374+Yetterko@users.noreply.github.com>
2025-04-30 12:39:10 +1000

    fix: resizing fix for defaultTimer.fxml

--- src/main/java/com/qut/cab302_project_pomodora/controller/DefaultTimerController.java
+++ src/main/java/com/qut/cab302_project_pomodora/controller/DefaultTimerController.java
@@ -59,6 +59,11 @@ public class DefaultTimerController extends ControllerSkeleton {
     public void initialize() throws SQLException, IOException {
         super.initialize();
 
+        contentPane.setPrefSize(DESIGN_WIDTH, DESIGN_HEIGHT);
+
+        contentPane.setMinSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        contentPane.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+
         Platform.runLater(() -> {
             navbarController.setNavButtonStyles(defaultTimer.getScene());
         });
